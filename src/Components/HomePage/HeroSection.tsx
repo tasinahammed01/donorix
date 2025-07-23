f0c22d7b5f283ade069730d04ed8e58f8d616684
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2025-07-24 00:03:56 +0600

    hero section responsive completed

--- src/Components/HomePage/HeroSection.tsx
+++ src/Components/HomePage/HeroSection.tsx
@@ -4,7 +4,7 @@ const HeroSection = () => {
     <div className="relative">
       {/* Background Image */}
       <img
-        className="w-full h-[80vh] object-cover"
+        className="lg:w-full lg:h-[80vh]  md:h-[100%] md:mb-0 mb-50  md:object-cover "
         src="https://i.ibb.co/hx7PpVYt/Red-White-Minimalist-Blood-Donation-Health-Banner-1.jpg"
         alt="Blood Donating image"
       />
@@ -14,20 +14,20 @@ const HeroSection = () => {
         initial={{ opacity: 0, x: 100 }} // from right
         animate={{ opacity: 1, x: 0 }}
         transition={{ duration: 1, ease: "easeOut" }}
-        className="absolute top-1/2 right-20 space-y-5 transform -translate-y-1/2 text-right max-w-xl"
+        className="absolute md:top-1/2 top-70 lg:right-20 right-10 space-y-5 transform -translate-y-1/2 text-right lg:max-w-xl md:max-w-sm" 
       >
-        <h1 className="text-6xl font-bold text-white drop-shadow-lg">
+        <h1 className="lg:text-6xl text-3xl font-bold text-white drop-shadow-lg">
           Welcome to Blood Bank
         </h1>
-        <p className="text-xl mt-4 text-white drop-shadow max-w-xl">
+        <p className="lg:text-xl text-sm mt-4 text-white drop-shadow lg:max-w-xl md:max-w-sm">
           Your blood can save other lives. Let's make a difference because every
           drop counts.
         </p>
-        <div className="flex gap-10 justify-end">
-          <button className="mt-6 px-6 py-3 bg-red-600 text-white rounded-full text-lg font-semibold shadow-md hover:bg-red-700 transition duration-300">
+        <div className="flex md:gap-10 gap-5 justify-end">
+          <button className="mt-6 lg:px-6 md:px-4 px-2 py-3 bg-red-600 text-white rounded-full md:text-lg font-semibold shadow-md hover:bg-red-700 transition duration-300">
             Want to be a Donator
           </button>
-          <button className="mt-6 px-6 py-3 border-2 border-red-600 hover:bg-red-600 text-white rounded-full text-lg font-semibold shadow-md transition duration-300">
+          <button className="mt-6 lg:px-6 md:px-4 px-2 py-3 border-2 border-red-600 hover:bg-red-600 text-white rounded-full md:text-lg font-semibold shadow-md transition duration-300">
             Need Blood
           </button>
         </div>
