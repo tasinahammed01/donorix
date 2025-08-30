b51badbfe91918de347f3a8011a61f7970c7b902
tasinahammed01 <tanjid.ahammed.01@gmail.com>
2025-08-30 23:19:13 +0600

    some issue with routes solved

--- src/SharedComponents/Header.tsx
+++ src/SharedComponents/Header.tsx
@@ -47,14 +47,14 @@ const Header = () => {
       ];
 
       if (auth.user.role === "donor")
-        links.push({ label: "Donor Dashboard", to: "/dashboard/donor" });
+        links.push({ label: "Donor Dashboard", to: "/dashboard/admin/donor" });
       if (auth.user.role === "recipient")
         links.push({
           label: "Recipient Dashboard",
           to: "/dashboard/recipient",
         });
       if (auth.user.role === "admin")
-        links.push({ label: "Admin Dashboard", to: "/dashboard/admin" });
+        links.push({ label: "Admin Dashboard", to: "/dashboard/admin/users" });
 
       links.push({ label: "Logout", to: "#", onClick: handleLogout });
 
